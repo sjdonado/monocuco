ea34f1d9b466f7f93ebad707c67c6a4982c49c1d
Juan Rodriguez <sjdonado@icloud.com>
2024-05-04 14:48:03 +0200

    refactor: update github buttons with react-github-btn

--- src/components/GithubButtons.tsx
+++ src/components/GithubButtons.tsx
@@ -1,31 +1,41 @@
+import GitHubButton from 'react-github-btn';
+
 export default function GithubButtons() {
   return (
     <div className="flex w-full justify-start items-end flex-col h-full p-3 text-xs mb-1.5">
-      <div className="flex gap-6">
-        <a
+      <div className="flex gap-2">
+        <GitHubButton
           href="https://github.com/sjdonado/monocuco/fork"
+          data-color-scheme="no-preference: light; light: light; dark: dark;"
           data-icon="octicon-repo-forked"
+          data-size="large"
           data-show-count="true"
           aria-label="Fork sjdonado/monocuco on GitHub"
         >
           Fork
-        </a>
-        <a
+        </GitHubButton>
+        <GitHubButton
           href="https://github.com/sjdonado/monocuco"
+          data-color-scheme="no-preference: light; light: light; dark: dark;"
           data-icon="octicon-star"
+          data-size="large"
           data-show-count="true"
           aria-label="Star sjdonado/monocuco on GitHub"
         >
           Star
+        </GitHubButton>
+      </div>
+      <div className="flex gap-2">
+        <p>¿Buscas añadir una palabra?</p>
+        <a
+          className="underline"
+          href="https://github.com/sjdonado/monocuco/blob/master/README.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click aquí
         </a>
       </div>
-      <a
-        href="https://github.com/sjdonado/monocuco/blob/master/README.md"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Buscas añadir una palabra? Click aquí
-      </a>
     </div>
   );
 }
