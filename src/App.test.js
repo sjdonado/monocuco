576f494544680e059ce4279e54a6c9e3acaaeb03
Juan Rodriguez <sjdonado@icloud.com>
2024-05-04 13:54:19 +0200

    refactor: migrate from react-scripts and babel to vite

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders monocuco title', () => {
-  const { getByText } = render(<App />);
-  const titleElement = getByText(/Monocuco/i);
-  expect(titleElement).toBeInTheDocument();
-});
